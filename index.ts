3c3a31cb0c0362cc8ce084b9555ec7538bef61d5
Guillaume Ongenae <guillaume.ongenae@gmail.com>
2020-10-01 21:22:22 +0200

    Refactor following http://disq.us/p/1l5d632

--- index.ts
+++ index.ts
@@ -1,53 +1,58 @@
 /**
  * Convert from latin to arabic numbers
  */
-export function toArabic(latin: string): number{
+export function toArabic(latin: string): number {
   return parseInt(latin, 10);
 }
 
-enum LatinToArabic {
-  M = 1000,
-  D = 500,
-  C = 100,
-  L = 50,
-  X = 10,
-  V = 5,
-  I = 1,
-  '' = 0,
-}
+const thousand: string[] = ["", "M", "MM", "MMM"];
+const hundred: string[] = [
+  "",
+  "C",
+  "CC",
+  "CCC",
+  "CD",
+  "D",
+  "DC",
+  "DCC",
+  "DCCC",
+  "CM",
+];
+const ten: string[] = [
+  "",
+  "X",
+  "XX",
+  "XXX",
+  "XL",
+  "L",
+  "LX",
+  "LXX",
+  "LXXX",
+  "XC",
+];
+const one: string[] = [
+  "",
+  "I",
+  "II",
+  "III",
+  "IV",
+  "V",
+  "VI",
+  "VII",
+  "VIII",
+  "IX",
+];
 
 /**
  * Convert from arabic numbers to latin
  */
 export function toLatin(arabic: number): string {
-  let latin = '';
+  let roman: string = "";
 
-  function convertStep(before: LatinToArabic | Number, current: LatinToArabic, next: LatinToArabic | 0) {
-    if (arabic >= before) {
-      return;
-    }
+  roman += thousand[Math.floor(arabic / 1000) % 10];
+  roman += hundred[Math.floor(arabic / 100) % 10];
+  roman += ten[Math.floor(arabic / 10) % 10];
+  roman += one[arabic % 10];
 
-    if (arabic >= current) {
-      latin += LatinToArabic[current];
-      arabic -= current;
-      return;
-    }
-
-    if (arabic >= current - next) {
-      latin += LatinToArabic[next] + LatinToArabic[current];
-      arabic -= current - next;
-    }
-  }
-
-  while (arabic > 0) {
-    convertStep(Number.POSITIVE_INFINITY, LatinToArabic.M, LatinToArabic.C);
-    convertStep(LatinToArabic.M, LatinToArabic.D, LatinToArabic.C);
-    convertStep(LatinToArabic.D, LatinToArabic.C, LatinToArabic.X);
-    convertStep(LatinToArabic.C, LatinToArabic.L, LatinToArabic.X);
-    convertStep(LatinToArabic.L, LatinToArabic.X, LatinToArabic.I);
-    convertStep(LatinToArabic.X, LatinToArabic.V, LatinToArabic.I);
-    convertStep(LatinToArabic.V, LatinToArabic.I, 0);
-  }
-
-  return latin;
+  return roman;
 }
